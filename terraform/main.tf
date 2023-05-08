94585830105a53d93a4ea1a35d061bd7b171aca7
LuxFer <david7arciniegas@gmail.com>
2023-05-08 10:46:38 -0500

    fix2

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 # Create an IAM role for the Lambda function to assume
 
 resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role"
+  name = "lambda_execution_role_2"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
